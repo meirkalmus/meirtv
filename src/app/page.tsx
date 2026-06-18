43f8ef134be4adae7d4255cd4808d2d1b7962dd9
MeirTV <dev@meirtv.com>
2026-06-18 11:12:25 +0000

    fix: force-dynamic on all DB pages to prevent build-time errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Suspense } from "react";
 import { searchShiurim, getFilterOptions } from "@/lib/queries";
 import ShiurCard from "@/components/ShiurCard";
